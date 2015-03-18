30cdb8e9d5c79ad3e3d2d2e0f128aada7163266f
Mark Shilton <mark.a.shilton@gmail.com>
2015-03-18 22:08:03 +0000

    overhauled chart code to get it working on more than one div at a time

--- js/chartCode.js
+++ js/chartCode.js
@@ -1,106 +1,70 @@
-/*
-This file includes the D3.js code to draw the 'racing dots' chart I use in my Look At The Stats! blog.
-You'll also need to load up D3 in your document to make this work. Check out D3 at http://www.d3js.org
-*/
+(
+  function() {
+    racingDots = function(params){ 
+    //Select the correct page element and then set up the svg canvas for us to draw on
+    var height = params.dotSpacing * params.listLength + (2 * params.svgPadding);
+    var width = params.width;
+    
+    var svg = d3.select(params.chart)
+            .append("svg")
+            .attr("height", height)
+            .attr("width", width)
+    
+    //Load the data
+    d3.json((params.data) + (params.refresh ? ("#" + Math.random()) : ""), function(error, json) {
+      if (error){console.log(error);}
+      else {var chartData = json;}
+    
+      //Draw the chart
 
-chartCode = function(){
+      //Set scale for data display      
+      var xScale = d3.scale.linear()
+              .domain([0, d3.max(chartData, function(d){return d.Gap;})])
+              .range([params.width - params.svgPadding, params.labelPadding + params.svgPadding]); 
 
-	function drawRacingDotsChart(divID){
-		
-		var divID = divID
-		var RankID = "Rank" + divID
-		var GapID = "Gap" + divID
+      //Define the x-axis
+      var xAxis = d3.svg.axis()
+              .scale(xScale)
+              .orient("bottom")
+              .ticks(5)
+              .tickSize("-" + params.height, 0, 0)
 
-		console.log(divID)
-		console.log(RankID + ", " + GapID)
+      //Draw the data points  
+      var circles = svg.selectAll("circle")
+              .data(chartData)
+              .enter()
+              .append("circle")
+              .attr("cx", d3.max(chartData, function(d){return d.Gap;}))
+              .transition().attr("cx", function(d){return xScale(d.Gap);}).duration(1000).delay(500)
+              .attr("cy", function(d, i){return (i * params.dotSpacing) + (params.dotSpacing/2);})
+              .attr("r", params.dotRadius + "px")
+              .attr("fill", "#2B8CBE")
 
-		//Set the view dimensions
-		var svgWidth = 480;
-		var listLength = 20;
-		var dotSpacing  = 20;
-		var svgPadding = 20;
-		var labelPadding = 150;
-		var svgHeight = dotSpacing * listLength + (2 * svgPadding);
+      //Label the data  points
+      var labels = svg.selectAll("text")
+              .data(chartData)
+              .enter()
+              .append("text")
+              .text(function(d){
+                if(d.Rank == 1){return d.Rank + ". " + d.name + ": " + d.Time;}
+                else {return d.Rank + ". " + d.name + ": +" + d.Gap;}
+                })
+              .attr("x", function(d){return xScale(d.Gap) - 7;})
+              .attr("y", function(d, i){return (i * params.dotSpacing) + ((params.dotSpacing/2) + (params.dotRadius/2));})
+              .attr("text-anchor", "end")
+              .attr("font-family", "sans-serif")
+              .attr("font-size", "10px")
+              .attr("opacity", 0).transition().attr("opacity", 1).duration(400).delay(1300);
 
-		var dotRadius = 4
-
-		//Viz code from here down. Add the json file reference here
-		d3.json("data/chartData.json",function(json){
-			console.log (json)
-
-			//Set up the SVG area
-			var svg = d3.select("chart")
-						.append("svg")
-						.attr("width", svgWidth)
-						.attr("height", svgHeight);
-
-			//Set scale for data display			
-			var xScale = d3.scale.linear()
-							.domain([0, d3.max(json, function(d){return d[GapID];})])
-							.range([svgWidth - svgPadding, labelPadding + svgPadding]); 
-
-			//Define the x-axis
-			var xAxis = d3.svg.axis()
-							.scale(xScale)
-							.orient("bottom")
-							.ticks(5)
-							.tickSize("-" + svgHeight, 0, 0)
-
-			//Draw the data points	
-			var circles = svg.selectAll("circle")
-							.data(json)
-							.enter()
-							.append("circle")
-							.attr("cx", function(d){
-								return xScale(d.Gap);
-								})
-							.attr("cy", function(d, i){
-								return (i * dotSpacing) + (dotSpacing/2);
-								})
-							.attr("r", dotRadius + "px")
-							.attr("fill", "#2B8CBE")
-							.on("mouseover", function(){
-									d3.select(this)
-										.attr("fill", "orange")	
-										.attr("r", "5px")
-										.append("title")
-										.text(function(d){return "+" + d[GapID]})
-							})
-							.on("mouseout", function(){
-									d3.select(this)
-										.attr("fill", "#2B8CBE")	
-										.attr("r", dotRadius + "px")
-							});
-
-			//Label the data  points
-			var labels = svg.selectAll("text")
-							.data(json)
-							.enter()
-							.append("text")
-							.text(function(d){
-								if(d.Rank == 1){	
-									return d.Rank + ". " + d.name + ": " + d.Time;
-									}
-								else {
-									return d.Rank + ". " + d.name;
-									}
-								})
-							.attr("x", function(d){
-								return xScale(d.Gap) - 7;
-								})
-							.attr("y", function(d, i){
-								return (i * dotSpacing) + ((dotSpacing/2) + (dotRadius/2));
-								})
-							.attr("text-anchor", "end")
-							.attr("font-family", "sans-serif")
-							.attr("font-size", "10px");
-
-			//Draw the X-axis
-			var drawXAxis = svg.append("g")
-								.attr("class", "axis")
-								.attr("transform", "translate(0, " + (listLength * dotSpacing) + ")")
-								.call(xAxis)				
-		});
-	};
-
-};
+      //Draw the X-axis
+      var drawXAxis = svg.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(0, " + (params.listLength * params.dotSpacing) + ")")
+                .call(xAxis)
+                .attr("text-anchor", "end")
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "10px");
+      })
+    }
+  }
+)();
